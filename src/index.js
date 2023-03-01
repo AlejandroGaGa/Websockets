f99564f146f41bcba9e7fd551e26c39f6ae92a42
AlejandroGaGa <alejandrogarciagallardo1234@gmail.com>
2023-02-28 19:00:42 -0600

    Create sockets and send client info to server

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import express from "express";
+import { Server as WebSocketServer } from "socket.io";
+import http from "http";
 
 const app = express();
+const server = http.createServer(app);
+const io = new WebSocketServer(server);
 
 app.use(express.static(__dirname + "/public"));
 
-app.listen(2000);
+io.on("connection", (socket) => {
+  console.log("Nueva conexión", socket.id);
+
+  socket.on("client:newnote", (data) => {
+    console.log(data);
+  });
+});
+
+server.listen(2000);
 
 console.log("Server listening on port", 2000);
--- src/public/maIn.js
+++ src/public/maIn.js
@@ -1,1 +1,14 @@
-console.log("Hello world!");
+const socket = io();
+
+const homework = document.querySelector("#homework");
+const title = document.querySelector("#title");
+const description = document.querySelector("#description");
+
+homework.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  socket.emit("client:newnote", {
+    title: title.value,
+    description: description.value,
+  });
+});
